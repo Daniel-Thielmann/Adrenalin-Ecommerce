2fe6f7b0afa2a968faedcafe0e1310c7001eba92
Daniel Thielmann <thielmanndan@gmail.com>
2024-01-17 23:18:33 -0300

    fixing bugs

--- components/know-adrenalin/card/index.tsx
+++ components/know-adrenalin/card/index.tsx
@@ -9,16 +9,16 @@ type KnowAdrenalinCardProps = {
 export default function KnowAdrenalinCard({ title, description, src }: KnowAdrenalinCardProps) {
     return (
         <div className="flex flex-col items-center justify-center max-w-sm 2xl:max-w-md 3xl:max-w-lg p-8 
-        transition-shadow duration-200 rounded-md hover:shadow-2xl gap-4">
+        transition-shadow transition-shadow-color duration-200 rounded-md hover:shadow-2xl gap-4">
             <Image
                 src={src}
                 alt={title}
                 width={150}
                 height={150}
-                className="max-h-[75] max-w-[75] xl:max-h-[120] xl:max-w-[120] 2xl:max-h-[220px] 2xl:max-w-[220px]"
+                className="max-h-[75] max-w-[75] xl:max-h-[150] xl:max-w-[150] 2xl:max-h-[220px] 2xl:max-w-[220px]"
             />
             <h1 className="text-center text-xl xl:text-2xl 2xl:text-3xl 3xl:text-4xl cursor-default">{title}</h1>
-            <p className="text-gray-500 text-lg lg:text-xl 2xl:text-2xl 3xl:text-3xl text-center line-clamp-10 cursor-default">{description}</p>
+            <p className="text-gray-400 text-lg lg:text-xl 2xl:text-2xl 3xl:text-3xl text-center line-clamp-10 cursor-default">{description}</p>
         </div>
     )
 }
