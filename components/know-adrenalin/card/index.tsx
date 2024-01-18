d4f744bd3235129aadc841af3f47b3ed340007d3
Daniel Thielmann <thielmanndan@gmail.com>
2024-01-17 22:57:25 -0300

    know adrenalin done

---
+++ components/know-adrenalin/card/index.tsx
@@ -0,0 +1,24 @@
+import Image from "next/image"
+
+type KnowAdrenalinCardProps = {
+    title: string;
+    description: string;
+    src: string;
+}
+
+export default function KnowAdrenalinCard({ title, description, src }: KnowAdrenalinCardProps) {
+    return (
+        <div className="flex flex-col items-center justify-center max-w-sm 2xl:max-w-md 3xl:max-w-lg p-8 
+        transition-shadow duration-200 rounded-md hover:shadow-2xl gap-4">
+            <Image
+                src={src}
+                alt={title}
+                width={150}
+                height={150}
+                className="max-h-[75] max-w-[75] xl:max-h-[120] xl:max-w-[120] 2xl:max-h-[220px] 2xl:max-w-[220px]"
+            />
+            <h1 className="text-center text-xl xl:text-2xl 2xl:text-3xl 3xl:text-4xl cursor-default">{title}</h1>
+            <p className="text-gray-500 text-lg lg:text-xl 2xl:text-2xl 3xl:text-3xl text-center line-clamp-6 cursor-default">{description}</p>
+        </div>
+    )
+}
--- components/know-adrenalin/index.tsx
+++ components/know-adrenalin/index.tsx
@@ -1,5 +1,6 @@
 import Title from "../title"
 import { IBM_Plex_Sans } from 'next/font/google'
+import KnowAdrenalinCard from "./card"
 
 const ibm = IBM_Plex_Sans({
     subsets: ['latin'],
@@ -8,10 +9,21 @@ const ibm = IBM_Plex_Sans({
 
 export default function KnowAdrenalin() {
     return (
-        <div className="w-full space-y-12">
+        <div className="w-full space-y-12 py-8 border-b border-t border-gray-300">
             <div className={ibm.className}>
-                <div className="text-gray-500">
+                <div className="text-gray-300">
                     <Title title="Conheça a Adrenalin" subtitle="Sobre" />
+                    <div className="flex justify-center">
+                        <KnowAdrenalinCard src="/icons/foxx.png" title="Melhores Marcas" description="Possuimos as melhores marcas para a sua aventura, 
+                        para você aproveitar o máximo. Saiba mais abaixo" />
+                        <KnowAdrenalinCard src="/icons/80.png" title="Melhores Ofertas" description="Aproveite o frete grátis para o Brasil todo
+                        e garanta sua compra com uma oferta especial! Descontos de até 80% na promoção relâmpago!"/>
+                        <KnowAdrenalinCard src="/icons/star.png" title="Avaliações" description="Sistema de avaliações em todos os produtos, confira
+                        a opinião de quem já comprou e garanta uma boa compra!" />
+                        <KnowAdrenalinCard src="/icons/camping.png" title="Para todos os esportes" description="Confira a variedade de
+                        categorias, seja você um piloto competitivo ou um hiker amador, temos todos os produtos que 
+                        você precisa."/>
+                    </div>
                 </div>
             </div>
         </div>
