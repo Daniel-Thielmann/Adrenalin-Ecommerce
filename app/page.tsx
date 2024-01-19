602ed244da464d99de283627f7d7899e1847d3ce
Daniel Thielmann <thielmanndan@gmail.com>
2024-01-18 23:35:17 -0300

    wallpaper division finished

--- app/page.tsx
+++ app/page.tsx
@@ -3,16 +3,21 @@ import BestProductsReverse from '@/components/best-products-reverse'
 import HeroSection from '@/components/hero-section'
 import KnowAdrenalin from '@/components/know-adrenalin'
 import Title from '@/components/title'
+import WallpaperDivision from '@/components/wallpaper-division'
+import Image from "next/image"
 
 export default function Home() {
   return (
-    <div className='flex flex-col gap-20 w-full md:w-10/12 py-8 px-4 md:p-0 min-h-screen mx-auto'>
-      <HeroSection />
-      <KnowAdrenalin />
-      <Title title="Em destaque" subtitle="Produtos mais vendidos" />
-      <BestProducts />
-      <BestProductsReverse />
-      <BestProducts />
+    <div>
+      <div className='flex flex-col gap-20 w-full md:w-10/12 py-8 px-4 md:p-0 min-h-screen mx-auto'>
+        <HeroSection />
+        <KnowAdrenalin />
+        <Title title="Destaques por categoria" />
+        <BestProducts />
+        <BestProductsReverse />
+        <BestProducts />
+      </div>
+      <WallpaperDivision />
     </div>
   )
 }
---
+++ components/wallpaper-division/index.tsx
@@ -0,0 +1,34 @@
+import { Zap } from "lucide-react"
+import Image from "next/image"
+import Link from "next/link"
+
+export default function WallpaperDivision() {
+    return (
+        <div className="mt-20">
+            <Link href='/' className='lg:col-span-3 relative group'>
+                <div className='relative overflow-hidden'>
+                    <Image
+                        src='/home/placeholder/kite.jpg'
+                        alt="imagem em destaque"
+                        width={1920}
+                        height={1080}
+                        className="cursor-pointer "
+                    />
+                    <div className='absolute inset-0 bg-gradient-to-b from-transparent to-black lg:opacity-0 lg:group-hover:opacity-100 transition-opacity
+                duration-300' />
+                </div>
+
+                <div className='absolute inset-0 flex items-center justify-center text-center bottom-0 p-6 text-white opacity-100 lg:opacity-0 lg:group-hover:opacity-100 transition-opacity duration-300'>
+                    <div className='flex flex-col gap-4'>
+                        <div className='flex items-center justify-center w-screen lg:bg-white/70 lg:rounded-lg lg:text-black'>
+                            <h3 className='text-xl md:text-2xl lg:text-3xl xl:text-4xl 2xl:text-5xl 3xl:text-6xl uppercase text-white lg:text-black'>
+                                Todas as Categorias
+                            </h3>
+                        </div>
+                    </div>
+                </div>
+
+            </Link>
+        </div>
+    )
+}
