10d7400e1c409a55e7b87565187bf5ea7148249e
Daniel Thielmann <thielmanndan@gmail.com>
2024-01-21 23:56:34 -0300

    fixing footer responsiveness

--- components/footer/index.tsx
+++ components/footer/index.tsx
@@ -1,12 +1,11 @@
 import Image from "next/image";
 import Link from "next/link";
-import { Just_Another_Hand } from 'next/font/google'
+import { Just_Another_Hand } from 'next/font/google';
 
 const jah = Just_Another_Hand({
     subsets: ['latin'],
     weight: '400'
-})
-
+});
 
 export default function Footer() {
     return (
@@ -16,44 +15,52 @@ export default function Footer() {
                     <div className={jah.className}>
                         <span className="text-6xl text-center">Adrenalin</span>
                     </div>
-                    <span className="text-sm text-center text-">2024 Adrenalin Todos os direitos reservados.</span>
-                    <span className="text-sm text-center text-">Desenvolvedor: Daniel Thielmann</span>
+                    <span className="text-sm text-center">2024 Adrenalin Todos os direitos reservados.</span>
+                    <span className="text-sm text-center">Desenvolvedor: Daniel Thielmann</span>
                 </div>
             </div>
 
             <div className="flex flex-row items-center justify-center space-x-8">
-                <Image
-                    src='/home/tech/tailwind.png'
-                    width={300}
-                    height={200}
-                    alt=''
-                    className="max-w-[14rem] lg:max-w-sm opacity-80 hover:opacity-100 transition-all"
-                />
-
-                <Image
-                    src='/home/tech/next.png'
-                    width={300}
-                    height={200}
-                    alt=''
-                    className="max-w-[14rem] lg:max-w-sm opacity-80 hover:opacity-100 transition-all"
-                />
-
-                <Image
-                    src='/home/tech/prisma.png'
-                    width={200}
-                    height={200}
-                    alt=''
-                    className="max-w-[14rem] lg:max-w-sm opacity-80 hover:opacity-100 transition-all"
-                />
-
-                <Link href='https://www.codejr.com.br' target="_blank">
+                <div className="max-w-[14rem] lg:max-w-sm">
                     <Image
-                        src='/logo/code.png'
+                        src='/home/tech/tailwind.png'
                         width={300}
                         height={200}
-                        alt='logo da codejr'
-                        className="max-w-[14rem] lg:max-w-sm opacity-80 hover:opacity-100 transition-all"
+                        alt=''
+                        className="w-full h-full object-cover opacity-80 hover:opacity-100 transition-all"
                     />
+                </div>
+
+                <div className="max-w-[14rem] lg:max-w-sm">
+                    <Image
+                        src='/home/tech/next.png'
+                        width={300}
+                        height={200}
+                        alt=''
+                        className="w-full h-full object-cover opacity-80 hover:opacity-100 transition-all"
+                    />
+                </div>
+
+                <div className="max-w-[14rem] lg:max-w-sm">
+                    <Image
+                        src='/home/tech/prisma.png'
+                        width={200}
+                        height={200}
+                        alt=''
+                        className="w-full h-full object-cover opacity-80 hover:opacity-100 transition-all"
+                    />
+                </div>
+
+                <Link href='https://www.codejr.com.br' target="_blank">
+                    <div className="max-w-[14rem] lg:max-w-sm">
+                        <Image
+                            src='/logo/code.png'
+                            width={300}
+                            height={200}
+                            alt='logo da codejr'
+                            className="w-full h-full object-cover opacity-80 hover:opacity-100 transition-all"
+                        />
+                    </div>
                 </Link>
             </div>
         </div>
