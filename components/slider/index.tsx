14ed7570d50e175f426fdec33b1db17e9f7ed819
Daniel Thielmann <thielmanndan@gmail.com>
2024-01-22 21:58:26 -0300

    slider fixed done

--- components/slider/index.tsx
+++ components/slider/index.tsx
@@ -2,13 +2,19 @@
 import { FC, useState } from "react";
 import { ArrowLeft, ArrowRight, Dot } from "lucide-react";
 
-const slides = [
-    '/home/best-products-side-image/trek.jpg',
-    '/home/best-products-side-image/utv.jpg',
-    '/home/best-products-side-image/original.jpg',
-    '/home/best-products-side-image/bs.jpg',
-    '/home/best-products-side-image/friends.jpg',
-    '/home/best-products-side-image/brandon.jpg',
+interface Slide {
+    src: string;
+    alt: string;
+    text: string;
+}
+
+const slides: Slide[] = [
+    { src: '/home/best-products-side-image/trek.jpg', alt: 'Imagem em destaque 1', text: 'Soooolta fml' },
+    { src: '/home/best-products-side-image/utv.jpg', alt: 'Imagem em destaque 2', text: 'Soooolta fml' },
+    { src: '/home/best-products-side-image/original.jpg', alt: 'Imagem em destaque 3', text: 'Soooolta fml' },
+    { src: '/home/best-products-side-image/bs.jpg', alt: 'Imagem em destaque 4', text: 'Soooolta fml' },
+    { src: '/home/best-products-side-image/friends.jpg', alt: 'Imagem em destaque 5', text: 'Soooolta fml' },
+    { src: '/home/best-products-side-image/brandon.jpg', alt: 'Imagem em destaque 6', text: 'Soooolta fml' },
 ];
 
 const Slider: FC = () => {
@@ -31,14 +37,17 @@ const Slider: FC = () => {
     }
 
     return (
-        <div className="max-w-[1200px] h-[780px] w-full m-auto py-16 px-4 relative group">
-            <div className="w-full h-full rounded-2xl relative overflow-hidden group-hover:after:opacity-100 group-hover:before:opacity-100 transition-opacity duration-300">
-                <div
-                    className="w-full h-full rounded-2xl bg-center bg-cover duration-500"
-                    style={{ backgroundImage: `url(${slides[currentIndex]})` }}
-                ></div>
-                {/* Gradiente que escurece a imagem */}
-                <div className="absolute inset-0 bg-gradient-to-b from-transparent to-black lg:opacity-0 lg:group-hover:opacity-100 transition-opacity
+        <div className="max-w-[900px] h-[700px] w-full m-auto py-16 px-4 relative group">
+            <div className="w-full h-full rounded-2xl relative overflow-hidden group-hover:after:opacity-100 group-hover:before:opacity-100 transition-opacity duration-500">
+                <div className="w-full h-full rounded-2xl bg-center bg-cover duration-500"
+                    style={{ backgroundImage: `url(${slides[currentIndex].src})` }}>
+                    <div className="absolute bottom-1/2 left-1/2 transform -translate-x-1/2 bg-black bg-opacity-50 text-white p-4 text-center text-xl opacity-100 lg:opacity-0 lg:group-hover:opacity-100 transition-opacity
+                duration-300">
+                        {slides[currentIndex].text}
+                    </div>
+                </div>
+
+                <div className="absolute inset-0 bg-gradient-to-b from-transparent to-black lg:opacity-0 lg:group-hover:opacity-70 transition-opacity
                 duration-300 before:content-[''] after:content-[''] group-hover:before:opacity-0 group-hover:after:opacity-0"></div>
             </div>
             <div className="hidden group-hover:block absolute top-[50%] -translate-x-0 translate-y-[-50%] left-5 text-2xl rounded-full p-2 bg-black/20 text-white cursor-pointer">
