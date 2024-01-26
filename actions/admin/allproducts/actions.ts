b413f7f0a20acd0d43fff2b56c3462f4a251be71
Daniel Thielmann <thielmanndan@gmail.com>
2024-01-26 20:41:05 -0300

    actions.ts allproducts crud

--- actions/admin/allproducts/actions.ts
+++ actions/admin/allproducts/actions.ts
@@ -1,1 +1,22 @@
-"use server";
+// actions.ts"use server";
+
+import prisma from "@/lib/db";
+
+export async function fetchAllProducts() {
+  const products = await prisma.product.findMany({
+    include: {
+      categories: {
+        select: {
+          id: true,
+          name: true,
+        },
+      },
+    },
+    orderBy: {
+      id: "asc",
+    },
+  });
+
+  const count = await prisma.product.count();
+  return { products, count };
+}
