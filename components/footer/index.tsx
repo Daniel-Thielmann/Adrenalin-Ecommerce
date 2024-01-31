17e73b43ab3658c140915029bb70dd019eccd7d5
Daniel Thielmann <thielmanndan@gmail.com>
2024-01-30 22:23:14 -0300

    footer bug fixed

--- components/footer/index.tsx
+++ components/footer/index.tsx
@@ -9,16 +9,26 @@ const jah = Just_Another_Hand({
 
 export default function Footer() {
     return (
-        <div className="bg-[#E3FC02] text-center w-full mt-20 pt-4 min-h-[250px] gap-8 items-center justify-center" style={{ userSelect: 'none' }}>
+        <div className="bg-[#E3FC02] flex text-center w-full mt-20 min-h-[250px] gap-8 items-center justify-center" style={{ userSelect: 'none' }}>
+            <Image
+                src={'/logo/adrenalin1.png'}
+                alt="logo do blog"
+                width={180}
+                height={180}
+                className="flex items-center h-25 w-25 rounded-xl hover: object-cover opacity-80 hover:opacity-100 transition-all"
+            />
+
             <div className="flex flex-col">
                 <div className="flex flex-col">
                     <div className={jah.className}>
                         <span className="text-6xl text-center">Adrenalin</span>
                     </div>
-                    <span className="text- text-center">2024 Adrenalin Todos os direitos reservados.</span>
                     <span className="text-lg text-center">Desenvolvedor: Daniel Thielmann</span>
+                    <span className="text- text-center">2024 Adrenalin Todos os direitos reservados.</span>
                 </div>
             </div>
+
+
 
             <div className="flex flex-row items-center justify-center space-x-8">
                 <div className="max-w-[14rem]">
