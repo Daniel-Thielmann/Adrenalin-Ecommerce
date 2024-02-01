bce42e12bbd638c4f6da9c495b96682e04209c39
Daniel Thielmann <thielmanndan@gmail.com>
2024-02-01 20:26:14 -0300

    new changes

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     images: {
-        domains: ['res.cloudinary.com', 'assets.specialized.com'],
+        domains: ['res.cloudinary.com', 'assets.specialized.com', 'img2.gratispng.com', 'hondacavalcantiprimo.com.br'],
     },
 }
 
