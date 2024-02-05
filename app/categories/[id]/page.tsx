560bb729acf886f56f5172a9549824863cc95519
Daniel Thielmann <thielmanndan@gmail.com>
2024-02-05 00:40:41 -0300

    trying deploy

--- app/categories/[id]/page.tsx
+++ app/categories/[id]/page.tsx
@@ -5,7 +5,7 @@ export default function CategoryPage() {
     return (
         <div>
             <Header />
-            <h1 className="text-white flex justify-center items-center text-4xl">work in progress...</h1>
+            <h1 className="text-white flex justify-center items-center text-4xl">work in progress....</h1>
             <Footer />
         </div>
     )
