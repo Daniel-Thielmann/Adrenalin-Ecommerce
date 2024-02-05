35bf26f25fd93bcbb4982c55d3a099a5b81e541f
Daniel Thielmann <thielmanndan@gmail.com>
2024-02-05 00:13:58 -0300

    work in progress

--- app/categories/[id]/page.tsx
+++ app/categories/[id]/page.tsx
@@ -1,7 +1,12 @@
+import Header from "@/components/header"
+import Footer from "@/components/footer"
+
 export default function CategoryPage() {
     return (
         <div>
-            <h1>work in progress...</h1>
+            <Header />
+            <h1 className="text-white flex justify-center items-center text-4xl">work in progress...</h1>
+            <Footer />
         </div>
     )
 }
